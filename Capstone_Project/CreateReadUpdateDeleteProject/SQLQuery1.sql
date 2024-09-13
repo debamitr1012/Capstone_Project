b0445f42da5665e3208378dd7309b1b1e8e7b934
Debayudh Mitra <78213067+debamitr1012@users.noreply.github.com>
2024-09-13 15:18:57 +0530

    Add files via upload

---
+++ Capstone_Project/CreateReadUpdateDeleteProject/SQLQuery1.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE users
+(
+id INT PRIMARY KEY IDENTITY(1,1),
+full_Name VARCHAR(MAX) NULL,
+gender VARCHAR(MAX) NULL,
+contact VARCHAR(MAX) NULL,
+email VARCHAR(MAX) NULL,
+bith_date DATE NULL,
+date_update DATE NULL
+)
+
+SELECT * from users
+
+INSERT INTO users (full_Name, gender, contact, email, bith_date)
+VALUES('Debayudh Mitra', 'Male', '5657836598', 'debaymitr@gmail.com', '2002-01-10')
---
+++ Capstone_Project/LoginRegistrationForm/SQLQuery2.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE admin
+(
+id INT PRIMARY KEY IDENTITY(1,1),
+email VARCHAR(MAX) NULL,
+username VARCHAR(MAX) NULL,
+pass VARCHAR(MAX) NULL,
+date_created VARCHAR(MAX) NULL
+)
